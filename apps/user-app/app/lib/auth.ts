cea7e357bfdb01d0bd10e1fa6dedaddf0bd8eec0
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-04-10 23:11:53 +0530

    User auth

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -19,8 +19,48 @@ export const authOptions = {
                     where: {
                         number: credentials.phone
                     }
-                })
-            }
+                });
+
+                if(existingUser){
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if(passwordValidation){
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        }
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        email: user.number
+                    }
+                } catch(e){
+                    console.error(e);
+                }
+
+                return null
+            },
         })
-    ]
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+
+        async session({token, session}: any){
+            session.user.id = token.sub
+
+            return session
+        }
+    }
 }
