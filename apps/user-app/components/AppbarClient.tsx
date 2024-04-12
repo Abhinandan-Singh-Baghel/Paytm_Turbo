1e64fa5f2d8e29e2285729b7de91595cda56a575
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-04-12 22:28:06 +0530

    Added Appbar imported from repo/ui

---
+++ apps/user-app/components/AppbarClient.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
+import { useRouter } from "next/navigation";
+
+
+
+export function AppbarClient(){
+    
+    const session = useSession();
+    const router = useRouter();
+
+    return (
+        <div>
+            <Appbar onSign={signIn} onSignout={async () => {
+                await signOut()
+                router.push("api/auth/signin")
+            }} user={session.data?.user} />
+        </div>
+    );
+}
