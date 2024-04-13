ef56a73f62aa9cfbd597256ace002d2179ebcbc7
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-04-13 22:40:12 +0530

    Appbar component

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,5 +1,30 @@
-export const Appbar = ({
+import { Button } from "./button"
 
-}) => {
+
+interface AppbarProps {
+    user?: {
+        name?: string | null;
+    },
     
+    onSignin: any,
+    onSignout: any
+}
+
+
+
+export const Appbar = ({
+    user,
+    onSignin,
+    onSignout
+}: AppbarProps) => {
+    
+    return <div>
+        <div>
+            PayTM
+        </div>
+        <div>
+            <Button onClick={user ? onSignout: onSignin}>{user ? "Logout": "Login"}</Button>
+        </div>
+    </div>
+
 }
