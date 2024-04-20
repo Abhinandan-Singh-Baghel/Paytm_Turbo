d65f9ca97a481150469c0654eccb628a062fc2ca
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-04-20 23:35:22 +0530

    Layout component for dashboard

---
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -0,0 +1,41 @@
+import { SidebarItem } from "../../components/SidebarItem";
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <div className="flex">
+        <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
+            <div>
+                <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
+                <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
+                <SidebarItem href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
+            </div>
+        </div>
+            {children}
+    </div>
+  );
+}
+
+
+function HomeIcon(){
+    return <div>
+
+    </div>
+}
+
+
+function TransferIcon(){
+    return <div>
+        
+    </div>
+}
+
+
+function TransactionsIcon(){
+    return <div>
+        
+    </div>
+}
