840dc9a55be5c5ac65c83505e0abfd6d47c45e76
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-04-30 23:13:22 +0530

    exporting addMoney component

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -2,8 +2,11 @@
 import { useState } from "react";
 import { Button } from "@repo/ui/button"
 import { Card } from "@repo/ui/card"
+import { Center } from "@repo/ui/center" 
+import { Select } from "@repo/ui/select"
+import { TextInput } from "@repo/ui/textinput"
+
 // import { Center } from "@repo/ui/center"
-import { CACHE_ONE_YEAR } from "next/dist/lib/constants";
 
 
 const SUPPORTED_BANKS = [{
@@ -17,6 +20,29 @@ const SUPPORTED_BANKS = [{
 export const AddMoney = () => {
     const [redirectUrl, setRedirectUrl ] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
     return <Card title="Add Money">
+        <div className="w-full">
+            <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
+
+            }} />
+
+            <div className="py-4 text-left">
+                Bank
+            </div>
+            <Select onSelect = {(value) => {
+                setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+            }} options={SUPPORTED_BANKS.map(x => ({
+                key: x.name,
+                value: x.name
+            }))} />
+            <div className="flex justify-center pt-4">
+                <Button onClick={() => {
+                    window.location.href = redirectUrl || "";
+                }}>
+                    Add Money
+                </Button>
+            </div>
+
+        </div>
 
     </Card>
 }
