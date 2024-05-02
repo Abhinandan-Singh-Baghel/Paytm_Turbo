29b25b3d64ab5c1d50e5089711ece9d5248f9dc1
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-05-02 22:08:05 +0530

    transfer page function

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -1,2 +1,18 @@
 import prisma from "@repo/db/client";
 import { AddMoney } from "@/components/AddMoneyCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+
+
+async function getBalance() {
+    const session = await getServerSession(authOptions);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
+}
