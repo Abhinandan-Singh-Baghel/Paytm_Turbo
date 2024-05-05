e829bb47cf57b2ae8e5ddb94e12325c4dfa4162c
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-05-05 23:32:39 +0530

    Adding Balance Card component to transfer page

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -38,11 +38,19 @@ export default async function(){
     const transactions = await getOnRampTransactions();
 
     return <div>
-        <div>
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
             Transfer
         </div>
-        <div>
-            
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div>
+                <AddMoney/>
+            </div>
+            <div>
+                <BalanceCard amount={balance.amount} locked={balance.locked} />
+                <div>
+                    <OnRampTransactions transactions={transactions} />
+                </div>
+            </div>
         </div>
     </div>
 }
