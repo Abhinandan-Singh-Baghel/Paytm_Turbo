014b164cc436caad7ee04817f019c0390fc44cef
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-05-13 22:27:24 +0530

    Add Money Card Component updated

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -5,6 +5,7 @@ import { Card } from "@repo/ui/card"
 import { Center } from "@repo/ui/center" 
 import { Select } from "@repo/ui/select"
 import { TextInput } from "@repo/ui/textinput"
+import { createOnRampTransaction } from "@/app/lib/actions/createOnRampTransactions";
 
 // import { Center } from "@repo/ui/center"
 
@@ -19,9 +20,12 @@ const SUPPORTED_BANKS = [{
 
 export const AddMoney = () => {
     const [redirectUrl, setRedirectUrl ] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name || "");
+    const [value, setValue] = useState(0)
     return <Card title="Add Money">
         <div className="w-full">
-            <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
+            <TextInput label={"Amount"} placeholder={"Amount"} onChange={(val) => {
+                setValue(Number(val))
 
             }} />
 
@@ -30,12 +34,15 @@ export const AddMoney = () => {
             </div>
             <Select onSelect = {(value) => {
                 setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+                setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.name || "");
+
             }} options={SUPPORTED_BANKS.map(x => ({
                 key: x.name,
                 value: x.name
             }))} />
             <div className="flex justify-center pt-4">
-                <Button onClick={() => {
+                <Button onClick={async () => {
+                    await createOnRampTransaction(provider, value)
                     window.location.href = redirectUrl || "";
                 }}>
                     Add Money
